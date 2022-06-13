e7bea0170aa9f0b88b0f1e46c84a65bb630670a3
tai-dang11 <78414238+tai-dang11@users.noreply.github.com>
2022-06-12 23:22:42 -0400

    Delete extract_model directory

--- extract_model/model_folder/predict.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-import numpy as np
-
-model = tf.keras.models.load_model('/Users/dttai11/Sudoku/logs')
-sudoku_numbers = np.load('/preprocess_image/nparray/ans.npy')
-out = model.predict(sudoku_numbers)
-puzzle=np.argmax(out,axis=-1).reshape((9,9))
-print(puzzle)
